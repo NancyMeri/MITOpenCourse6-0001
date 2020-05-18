9d3fc092b9571305ac661ecde70ab796686e16ad
NancyMeri <56763619+NancyMeri@users.noreply.github.com>
2020-05-18 15:05:16 -0400

    Add files via upload

---
+++ pset0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 17:18:01 2020
+
+@author: nancy
+"""
+#Problem set 0
+import numpy
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y = " + str(x**y))
+print("log(x) = " + str(numpy.log2(x)))
